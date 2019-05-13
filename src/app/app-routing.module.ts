d8d8c796d06edc36524e3641926722ace2ee2daf
Liying Huang <lxh37@psu.edu>
2019-05-13 17:21:09 -0400

    Update route, so click button will direct to a simple survey page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { InitiatedDrinkComponent } from './survey/initiated-drink/initiated-drink.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'survey/initated-drink', component: InitiatedDrinkComponent }
 ];
 
 @NgModule({
