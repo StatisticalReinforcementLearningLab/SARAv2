78c31256864ca92738fc2a596d4c3f683672dee0
Liying Huang <lxh37@psu.edu>
2019-05-13 17:21:09 -0400

    Update route, so click button will direct to a simple survey page

--- src/app/survey/survey.module.ts
+++ src/app/survey/survey.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { PromptedSurveyComponent } from './prompted-survey/prompted-survey.component';
 import { MorningReportComponent } from './morning-report/morning-report.component';
 import { InitiatedDrinkComponent } from './initiated-drink/initiated-drink.component';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [InitiatedDrinkComponent,MorningReportComponent,PromptedSurveyComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule.forRoot(),
+    FormsModule
   ],
   exports:[
     InitiatedDrinkComponent,
