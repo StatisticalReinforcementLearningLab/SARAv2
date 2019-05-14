3c525aa7e6b7310a3ea4518541075bbbc3148e24
Liying Huang <lxh37@psu.edu>
2019-05-14 15:16:32 -0400

    Add award part

--- src/app/incentive/incentive.module.ts
+++ src/app/incentive/incentive.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AwardComponent } from './award/award.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AwardComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[AwardComponent]
 })
 export class IncentiveModule { }
