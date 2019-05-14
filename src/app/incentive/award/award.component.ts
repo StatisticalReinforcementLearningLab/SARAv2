42d611363b1c91f08806e5a3ed98d1e272bf2552
Liying Huang <lxh37@psu.edu>
2019-05-14 15:33:31 -0400

    update package supported files

---
+++ src/app/incentive/award/award.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-award',
+  templateUrl: './award.component.html',
+  styleUrls: ['./award.component.scss'],
+})
+export class AwardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
