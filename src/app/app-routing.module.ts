eebde7f96f04b2763b89d2c567fe11b2d92d50c0
lxh37 <lxh37h@gmail.com>
2019-05-29 16:54:45 -0400

    Add memes as another incentive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { InitiatedDrinkComponent } from './survey/initiated-drink/initiated-drink.component';
 import { AwardComponent } from './incentive/award/award.component';
+import { AwardMemesComponent } from './incentive/award-memes/award-memes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'survey/initated-drink', component: InitiatedDrinkComponent },
-  { path: 'incentive/award', component: AwardComponent }
+  { path: 'incentive/award', component: AwardMemesComponent }
 ];
 
 @NgModule({
--- src/app/incentive/incentive.module.ts
+++ src/app/incentive/incentive.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AwardComponent } from './award/award.component';
+import { AwardMemesComponent } from './award-memes/award-memes.component';
 
 @NgModule({
-  declarations: [AwardComponent],
+  declarations: [AwardComponent, AwardMemesComponent],
   imports: [
     CommonModule
   ],
-  exports:[AwardComponent]
+  exports:[AwardComponent, AwardMemesComponent]
 })
 export class IncentiveModule { }
