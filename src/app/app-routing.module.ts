fb9cb68f563d3d818ee8c40c0e48a9b423fa7a9a
Mash <mashfiqui.r.s@gmail.com>
2019-08-21 17:37:58 -0400

    Added Dynamic survey

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AwardMemesComponent } from './incentive/award-memes/award-memes.compone
 import { ActivetaskComponent } from './survey/activetask/activetask.component';
 import { ActiveTask2Component } from './survey/active-task2/active-task2.component';
 import { VisualizationComponent } from './incentive/visualization/visualization.component';
+import { DynamicSurveyComponent } from './survey/dynamic-survey/dynamic-survey.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -17,7 +18,7 @@ const routes: Routes = [
   { path: 'incentive/award-memes', component: AwardMemesComponent },
   { path: 'survey/activetask', component: ActivetaskComponent },
   { path: 'survey/activetask2', component: ActiveTask2Component },
-    
+  { path: 'survey/dynamicsurvey', component: DynamicSurveyComponent },  
 ];
 
 @NgModule({
