1e78d05928e6f77219fd08f128cb4c2fd941f9b0
Mash <mashfiqui.r.s@gmail.com>
2019-08-22 21:22:58 -0400

    Added encryption and decryption

---
+++ src/app/storage/encrdecrservice.service.ts
@@ -0,0 +1,97 @@
+import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EncrDecrService {
+  constructor() { }
+ 
+  //The set method is use for encrypt the value.
+  set(keys, value){
+    var key = CryptoJS.enc.Utf8.parse(keys);
+    var iv = CryptoJS.enc.Utf8.parse(keys);
+    var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(value.toString()), key,
+    {
+        keySize: 128 / 8,
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+
+    return encrypted.toString();
+  }
+
+  //The get method is use for decrypt the value.
+  get(keys, value){
+    var key = CryptoJS.enc.Utf8.parse(keys);
+    var iv = CryptoJS.enc.Utf8.parse(keys);
+    var decrypted = CryptoJS.AES.decrypt(value, key, {
+        keySize: 128 / 8,
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+
+    return decrypted.toString(CryptoJS.enc.Utf8);
+  }
+
+
+  encrypt(msg, pass) {
+
+    // Code goes here
+    var keySize = 256;
+    var ivSize = 128;
+    var iterations = 100;
+
+    //var message = "Hello World";
+    //var password = "Secret Password";
+    var salt = CryptoJS.lib.WordArray.random(128 / 8);
+
+    var key = CryptoJS.PBKDF2(pass, salt, {
+        keySize: keySize / 32,
+        iterations: iterations
+    });
+
+    var iv = CryptoJS.lib.WordArray.random(128 / 8);
+
+    var encrypted = CryptoJS.AES.encrypt(msg, key, {
+        iv: iv,
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+
+    });
+
+    // salt, iv will be hex 32 in length
+    // append them to the ciphertext for use  in decryption
+    var transitmessage = salt.toString() + iv.toString() + encrypted.toString();
+    return transitmessage;
+  }
+
+  decrypt(transitmessage, pass) {
+
+    // Code goes here
+    var keySize = 256;
+    var ivSize = 128;
+    var iterations = 100;
+
+    var salt = CryptoJS.enc.Hex.parse(transitmessage.substr(0, 32));
+    var iv = CryptoJS.enc.Hex.parse(transitmessage.substr(32, 32))
+    var encrypted = transitmessage.substring(64);
+
+    var key = CryptoJS.PBKDF2(pass, salt, {
+        keySize: keySize / 32,
+        iterations: iterations
+    });
+
+    var decrypted = CryptoJS.AES.decrypt(encrypted, key, {
+        iv: iv,
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return decrypted.toString(CryptoJS.enc.Utf8);
+  }
+
+
+}
+
