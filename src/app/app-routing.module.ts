863e6439f9e59a3b0bc81110fbe0d98c0f56bf23
lxh37 <lxh37@psu.edu>
2019-10-01 15:27:58 -0400

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,21 +8,25 @@ import { ActiveTask2Component } from './survey/active-task2/active-task2.compone
 import { VisualizationComponent } from './incentive/visualization/visualization.component';
 import { DynamicSurveyComponent } from './survey/dynamic-survey/dynamic-survey.component';
 import { DemoAquariumComponent } from './incentive/aquarium/demo-aquarium/demo-aquarium.component';
+import { Q1MotivatedComponent } from './incentive/life-insights/q1-motivated/q1-motivated.component';
 import { SampleSurveyComponent } from './survey/sample-survey/sample-survey.component';
+import { SampleLifeInsightsComponent } from './incentive/life-insights/sample-life-insights/sample-life-insights.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'survey/initated-drink', component: InitiatedDrinkComponent },
-  { path: 'incentive/award', component: AwardComponent },
-  { path: 'incentive/award-memes', component: AwardMemesComponent },
-  { path: 'incentive/visualization', component: VisualizationComponent},
-  { path: 'incentive/award-memes', component: AwardMemesComponent },
-  { path: 'survey/activetask', component: ActivetaskComponent },
+  //{ path: 'survey/activetask', component: ActivetaskComponent },
   { path: 'survey/activetask2', component: ActiveTask2Component },
   { path: 'survey/dynamicsurvey', component: DynamicSurveyComponent }, 
   { path: 'survey/samplesurvey', component: SampleSurveyComponent }, 
+  { path: 'incentive/award', component: AwardComponent },
+  { path: 'incentive/award-memes', component: AwardMemesComponent },
+  { path: 'incentive/visualization', component: VisualizationComponent},
   { path: 'incentive/aquariumone', component: DemoAquariumComponent },  
+  { path: 'life-insight/q1lifeinsight', component: Q1MotivatedComponent },
+  { path: 'incentive/sample-life-insight', component: SampleLifeInsightsComponent } 
+
 ];
 
 @NgModule({
