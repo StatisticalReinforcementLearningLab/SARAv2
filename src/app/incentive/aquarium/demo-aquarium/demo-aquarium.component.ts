43c7bcfc5aa2a641f02ab2176c374df41f047da2
lxh37 <lxh37@psu.edu>
2019-10-01 15:31:30 -0400

    Add files via upload

--- src/app/incentive/aquarium/demo-aquarium/demo-aquarium.component.ts
+++ src/app/incentive/aquarium/demo-aquarium/demo-aquarium.component.ts
@@ -3,8 +3,12 @@ import { Boot } from '../fishgame/Boot';
 import { Preloader } from '../fishgame/Preloader';
 import { GameSmall } from '../fishgame/GamesSmall2';
 import { GameOver } from '../fishgame/GameOver';
-import { Router } from '@angular/router';
+import { Game } from '../fishgame/Game';
+import { Level1 } from '../fishgame/Level1';
+import { Level1Small } from '../fishgame/Level1Small';
 import { FormsModule } from '@angular/forms';
+//import { PickGameService } from './pick-game.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 declare let Phaser: any;
 
@@ -16,24 +20,20 @@ declare let Phaser: any;
 export class DemoAquariumComponent implements OnInit {
 
   game;
+  pickedGame ;
+  totalPoints = 0;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router, 
+    //private pickGameService: PickGameService,
+    private route: ActivatedRoute) { 
     console.log("Constructor called");
     
-    this.game =  new Phaser.Game(
-      window.innerWidth, 700,
-      Phaser.AUTO,
-      'gameDiv'
-    );
-    this.game.state.add('Boot', Boot);
-    this.game.state.add('Preloader', Preloader);
-    this.game.state.add('GameSmall', GameSmall);
-    this.game.state.add('GameOver', GameOver);
-    this.game.state.start('Boot');
-    //self = this;
-
-    this.game.state.states["GameSmall"].assignscope(this);
-
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.totalPoints = this.router.getCurrentNavigation().extras.state.totalPoints;
+        console.log("Pass totalPoints: "+this.totalPoints);
+      }
+    });
   }
 
 
@@ -56,6 +56,56 @@ export class DemoAquariumComponent implements OnInit {
     s.rotation = 0.14;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.game =  new Phaser.Game(
+      window.innerWidth, 700,
+      Phaser.AUTO,
+      'gameDiv'
+    );
+
+    this.game.state.add('Boot', Boot);
+    var preLoader = new Preloader();
+
+    if(this.totalPoints <770 && this.totalPoints >= 0){
+      preLoader.setGameName(this.pickedGame = "GameSmall");
+      this.game.state.add('Preloader', preLoader);
+      var gameSmall = new GameSmall();
+      gameSmall.setTotalPoints(this.totalPoints);
+      this.game.state.add('GameSmall', gameSmall);
+    } else if ( this.totalPoints >=770 && this.totalPoints <1060 ){
+      preLoader.setGameName(this.pickedGame = "Game");
+      this.game.state.add('Preloader', preLoader);
+      var game = new Game();
+      game.setTotalPoints(this.totalPoints);
+      this.game.state.add('Game', game);
+    } else if( this.totalPoints >=1060 && this.totalPoints <1710 ){
+      preLoader.setGameName(this.pickedGame = "Level1Small");
+      this.game.state.add('Preloader', preLoader);
+      var level1Small = new Level1Small();
+      level1Small.setTotalPoints(this.totalPoints);
+      this.game.state.add('Level1Small', level1Small);
+    } else if( this.totalPoints >=1710 ){
+      preLoader.setGameName(this.pickedGame = "Level1");
+      this.game.state.add('Preloader', preLoader);
+      var level1 = new Level1();
+      level1.setTotalPoints(this.totalPoints);
+      this.game.state.add('Level1', level1);
+    }
+    else {
+      preLoader.setGameName(this.pickedGame = "GameOver");
+      this.game.state.add('Preloader', preLoader);
+    }
+    this.game.state.add('GameOver', GameOver);
+    this.game.state.start('Boot');
+    //self = this;
+
+    this.game.state.states[this.pickedGame].assignscope(this);
+
+    //this.pickGameService.currentGame.subscribe(game => this.pickedGame = game)
+  }
+ 
+  ionViewDidLeave(){
+    this.game.destroy();
+  }
 
 }
