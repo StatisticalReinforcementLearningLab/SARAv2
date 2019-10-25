e37aee01f0fa81b3b6307c74016c4a5752b194cb
lxh37 <lxh37h@gmail.com>
2019-10-25 16:00:23 -0400

    add comments

--- src/app/notification/notification.module.ts
+++ src/app/notification/notification.module.ts
@@ -1,3 +1,12 @@
+/*
+
+Install following, then this module shall work.
+ionic cordova plugin add onesignal-cordova-plugin
+npm install @ionic-native/onesignal
+
+*/
+
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OneSignalService } from './one-signal.service';
