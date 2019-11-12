e0aab41f321e26567a259ed3fb6e5b6da2063739
lxh37 <lxh37h@gmail.com>
2019-11-12 14:52:20 -0500

    Save 7-day life-insights for relevent questions and drop-down list to switch chart.

--- src/app/incentive/life-insights/life-insights.module.ts
+++ src/app/incentive/life-insights/life-insights.module.ts
@@ -1,9 +1,16 @@
+
+/* 
+    The profile for questions is stored in /assets/data/life_insight.json
+*/
+
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { Q1MotivatedComponent } from './q1-motivated/q1-motivated.component';
 import { SampleLifeInsightsComponent } from './sample-life-insights/sample-life-insights.component';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 const routes: Routes = [
     { path: 'sample-life-insights', component: SampleLifeInsightsComponent }
@@ -13,6 +20,7 @@ const routes: Routes = [
   declarations: [Q1MotivatedComponent,SampleLifeInsightsComponent],
   imports: [
     CommonModule,
+    FormsModule,
     IonicModule.forRoot(),
     RouterModule.forChild(routes)
   ],
