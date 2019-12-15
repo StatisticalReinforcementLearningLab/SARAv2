e14e93481b3a94c287aec3fd7e97d9f91e5ec972
Mash <mashfiqui.r.s@gmail.com>
2019-12-15 16:21:33 -0500

    Android Api 28 level issue, Added paused.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-//import { PreLoad } from '../PreLoad';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { DemoAquariumComponent } from '../incentive/aquarium/demo-aquarium/demo-aquarium.component';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,44 @@ import { Component } from '@angular/core';
 })
 
 //@PreLoad('aquarium')
-export class HomePage {
+export class HomePage implements OnInit {
 
+  private sub1$:any;
+  private sub2$:any;
+
+  @ViewChild(DemoAquariumComponent) child;
+
+  constructor(private platform: Platform) { 
+    console.log("Constructor called");
+    this.sub1$=this.platform.pause.subscribe(() => {        
+      console.log('****UserdashboardPage PAUSED****');
+      this.child.pauseGameRendering();
+    });  
+    this.sub2$=this.platform.resume.subscribe(() => {      
+      console.log('****UserdashboardPage RESUMED****');
+      this.child.resumeGameRendering();
+    });
+  }
+
+  ionViewDidLeave(){
+    console.log("ionDidLeave");
+    this.child.ionViewDidLeaveFunction();
+  }
+
+  ionViewDidEnter() {
+    this.child.loadFunction();
+  }
+
+  ionViewWillUnload() {
+    this.sub1$.unsubscribe();
+    this.sub2$.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    //throw new Error("Method not implemented.");
+  }
   
+
+
 
 }
