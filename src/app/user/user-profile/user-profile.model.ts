66a8839a9c66dd1e48715e3ec11e93a1a9aa177a
Dan Atkins <atkinsdj@chop.edu>
2020-02-25 19:05:59 -0500

    Addedwww, platforms, and plugins folders to .gitignore In dynamic survey, moved userProfileService.surveyCompleted to after call to award-dollar service. In UserProfileService, saving award dollars and dates to UserProfile from local storage in surveyCompleted and populating corresponding local storage with award dollars and dates when UserProfile is loaded. Added addReinforcementData function to UserProfileService
    Former-commit-id: 6e5c6a549035d144be9abae1a0755fa15f41fe84

--- src/app/user/user-profile/user-profile.model.ts
+++ src/app/user/user-profile/user-profile.model.ts
@@ -10,7 +10,8 @@ export class UserProfile {
     public life_insights = {"daily_survey": {}};
     public reinfrocement_data = {};
 
-    public oneSignalPlayerId?: string;                     
+    public oneSignalPlayerId?: string;     
+    public AwardDollarDates?: {};                
 
     public lastupdate: number;
     public readable_ts: string;
