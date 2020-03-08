dfef7047213020a6bd47a7fa64f4b4b167eca7c4
Mash <mashfiqui.r.s@gmail.com>
2020-03-08 14:53:00 -0700

    Add falling love signs. Both exploding and falling direction.

--- src/app/incentive/award-memes/award-memes.component.ts
+++ src/app/incentive/award-memes/award-memes.component.ts
@@ -19,7 +19,6 @@ export class AwardMemesComponent implements OnInit {
 
   viewWidth = 512;
   viewHeight = 350;
-  drawingCanvas = <HTMLCanvasElement>document.getElementById("drawing_canvas");
   ctx;
   timeStep = (1/60);
 
@@ -45,10 +44,8 @@ export class AwardMemesComponent implements OnInit {
     this.ga.trackView('Life-insight')
     .then(() => {console.log("trackView at Life-insight!")})
     .catch(e => console.log(e));
-
-
-    //this.drawConfetti();
   }
+  
 
   ngAfterViewInit() {
 
@@ -59,19 +56,23 @@ export class AwardMemesComponent implements OnInit {
       this.meme_data = await res.json();
       this.showmemes();
     });
+
+    
+    
   }
 
   showmemes(){
     //window.localStorage['meme_shuffle5'] = "[]";
     //var randomInt = Math.floor(Math.random() * this.meme_data.length);
     //this.whichImage = "./assets/memes/"+this.meme_data[randomInt]["filename"];
-    console.log('Meme data: ' + JSON.stringify(this.meme_data));
+    //console.log('Meme data: ' + JSON.stringify(this.meme_data));
     this.meme_data = this.shuffle(this.meme_data);
-    console.log('Meme suffled: ' + JSON.stringify(this.meme_data));
+    //console.log('Meme suffled: ' + JSON.stringify(this.meme_data));
     var picked_meme = this.pick_meme(this.meme_data);
-    console.log('picked_meme: ' + JSON.stringify(picked_meme));
+    //console.log('picked_meme: ' + JSON.stringify(picked_meme));
     this.whichImage = "./assets/memes/"+picked_meme[0]["filename"];
     this.reinforcementObj['reward_img_link'] = "/memes/"+picked_meme[0]["filename"];
+    setTimeout(e => this.drawImageOnCanvas(this.whichImage), 200);
   }
   
   ratingChanged(rating){
@@ -132,10 +133,12 @@ export class AwardMemesComponent implements OnInit {
 
   //
   initDrawingCanvas() {
+    /*
     this.drawingCanvas.style.width = '100%';
     this.drawingCanvas.width = this.drawingCanvas.offsetWidth;
     this.drawingCanvas.style.height = '90%';
     this.ctx = this.drawingCanvas.getContext('2d');
+    */
 
     //createLoader();
     //createExploader();
@@ -146,4 +149,170 @@ export class AwardMemesComponent implements OnInit {
   drawConfetti(){
     this.initDrawingCanvas();
   }
+
+  drawImageOnCanvas(imageF_file_path) {
+
+    //
+    var imageObj = new Image();
+    imageObj.src = imageF_file_path;
+    
+    //
+    var drawingCanvas = <HTMLCanvasElement>document.getElementById("drawing_canvas");
+    drawingCanvas.style.width = '100%';
+    drawingCanvas.width = drawingCanvas.offsetWidth;
+   
+    var ctx = drawingCanvas.getContext('2d');
+   
+    //
+    imageObj.onload = function () {
+      console.log("print: " + (imageObj.height/imageObj.width));
+      drawingCanvas.height = Math.abs(drawingCanvas.width*(imageObj.height/imageObj.width));
+
+      ctx.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height, // source rectangle
+        0, 0, drawingCanvas.width, drawingCanvas.height); // destination rectangle
+
+      //
+      
+    }
+    this.HeartsBackground.initialize(drawingCanvas);
+
+    
+  }
+
+
+
+
+  HeartsBackground = {
+    heartHeight: 60,
+    heartWidth: 64,
+    hearts: [],
+    heartImage: './assets/img/valentinesheart.png',
+    maxHearts: 30,
+    minScale: 0.4,
+    draw: function() {
+      //this.setCanvasSize();
+      this.ctx.clearRect(0, 0, this.w, this.h);
+      //console.log("Hearts draw function called");
+      var ctx = this.ctx;
+      for (var i = 0; i < this.hearts.length; i++) {
+        var heart = this.hearts[i];
+        heart.image = new Image();
+        heart.image.style.height = heart.height;
+        heart.image.src = this.heartImage;
+        ctx.drawImage(heart.image, heart.x, heart.y, heart.width, heart.height);
+      }
+      this.move();
+    },
+    move: function() {
+      //console.log("Move function called");
+      for(var b = 0; b < this.hearts.length; b++) {
+        var heart = this.hearts[b];
+        heart.y += heart.ys;
+        if(heart.y > this.h) {
+          //heart.x = Math.random() * this.w;
+          //heart.y = -1 * this.heartHeight;
+        }
+      }
+    },
+    angularDraw: function() {
+      //this.setCanvasSize();
+      this.ctx.clearRect(0, 0, this.w, this.h);
+      //console.log("Hearts draw function called");
+      var ctx = this.ctx;
+      for (var i = 0; i < this.hearts.length; i++) {
+        var heart = this.hearts[i];
+        heart.image = new Image();
+        heart.image.style.height = heart.height;
+        heart.image.src = this.heartImage;
+        ctx.drawImage(heart.image, heart.angle_x, heart.angle_y, heart.width, heart.height);
+      }
+      this.angularMove();
+    },
+    angularMove: function() {
+      //console.log("Move function called");
+      for(var b = 0; b < this.hearts.length; b++) {
+        var heart = this.hearts[b];
+        //heart.y += heart.ys;
+        //console.log("" + heart.angle_x + "," + heart.angle_y);
+        //console.log(heart.angle);
+        //console.log("" + heart.angle_x + "," + heart.angle_y + ", " + heart.angle_deltax + ", " + heart.angle_deltay);
+        heart.angle_x += heart.angle_deltax;
+        heart.angle_y += heart.angle_deltay;
+        //console.log("" + heart.angle_x + "," + heart.angle_y);
+        if(heart.y > this.h) {
+          //heart.x = Math.random() * this.w;
+          //heart.y = -1 * this.heartHeight;
+        }
+      }
+    },
+    setCanvasSize: function() {
+      this.canvas.width = window.innerWidth;
+      this.canvas.height = window.innerHeight - 170;
+      console.log("Set canvas size function called");
+      this.w = this.canvas.width;
+      this.h = this.canvas.height;
+    },
+    initialize: function(drawingCanvas) {
+      console.log("Initialize hearts");
+      this.canvas = <HTMLCanvasElement>document.getElementById("hearts_canvas"); //$('#canvas')[0]; hearts_canvas
+      if(!this.canvas.getContext)
+        return;
+
+      this.setCanvasSize();
+      this.ctx = this.canvas.getContext('2d'); 
+
+      this.canvas.addEventListener("touchstart",  function(event) {event.preventDefault()});
+      this.canvas.addEventListener("touchmove",   function(event) {event.preventDefault()});
+      this.canvas.addEventListener("touchend",    function(event) {event.preventDefault()});
+      this.canvas.addEventListener("touchcancel", function(event) {event.preventDefault()});
+      // Attach an event handler to the document
+      //this.canvas.addEventListener("mousemove",   function(event) {event.preventDefault()});
+  
+      for(var a = 0; a < this.maxHearts; a++) {
+        var scale = (Math.random() * (1 - this.minScale)) + this.minScale;
+        this.hearts.push({
+          x: Math.random() * this.w,
+          y: Math.random() * this.h,
+          ys: Math.random() + 8,
+          height: scale * this.heartHeight,
+          width: scale * this.heartWidth,
+          angle_x: this.w/2,
+          angle_y: this.h/2,
+          angle_deltax: this.getRandomArbitraryMoreThanX(-10,10,6),
+          angle_deltay: this.getRandomArbitraryMoreThanX(-10,10,6),
+          opacity: scale
+        });
+      }
+  
+      //setInterval($.proxy(this.draw, this), 30);
+      //setTimeout(e => this.draw, 30);
+      //this.draw();
+      var intervalVar;
+      if(Math.random()>0.5)
+          intervalVar = setInterval(e => this.angularDraw(), 30);
+      else
+          intervalVar = setInterval(e => this.draw(), 30);
+
+      setTimeout(e => this.stopInterval(intervalVar), 1200);
+
+    },
+    stopInterval(intervalVar) {
+      this.ctx.clearRect(0, 0, this.w, this.h);
+      clearInterval(intervalVar);
+    },
+    getRandomArbitrary(min, max) {
+      return Math.random() * (max - min) + min;
+    },
+    getRandomArbitraryMoreThanX(min, max, X) {
+      var rand_var =  this.getRandomArbitrary(min, max);
+      if(Math.abs(rand_var) < X){
+        if(rand_var < X)
+          return  rand_var-X;
+        if(rand_var > X)
+          return  rand_var+X;
+      }else{
+        return rand_var;
+      }
+    }
+  };
 }
