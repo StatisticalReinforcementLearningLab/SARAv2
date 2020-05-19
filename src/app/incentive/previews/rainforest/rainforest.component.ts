a7e2fc77b06d49d9e5dbd434d64211b9461bf894
Mash <mashfiqui.r.s@gmail.com>
2020-05-18 21:16:05 -0700

    Changes: - Added a history of inspirational quotes - Moved contact, and info pages to menu bar. - Add list of fish/animal at the bottom - Added new progress bars

---
+++ src/app/incentive/previews/rainforest/rainforest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rainforest',
+  templateUrl: './rainforest.component.html',
+  styleUrls: ['./rainforest.component.css']
+})
+export class RainforestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
