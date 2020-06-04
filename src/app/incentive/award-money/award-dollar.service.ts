c777900f5b6f99858dc6bf47767e78a30dd43397
Mash <mashfiqui.r.s@gmail.com>
2020-06-03 23:15:04 -0700

    refactored the money code.
    Former-commit-id: 8044d2ebf91641e0f4d1ad579637b6ef129d66c4

--- src/app/incentive/award-money/award-dollar.service.ts
+++ src/app/incentive/award-money/award-dollar.service.ts
@@ -4,133 +4,203 @@ import * as moment from 'moment';
 import { UserProfileService } from 'src/app/user/user-profile/user-profile.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AwardDollarService {
+    
+    usersCurrentDollars;
+    users3DayStreakHistory;
+    
+    constructor(private userProfileService: UserProfileService) { 
+    }
+    
+    
+    getCurrentlyEarnedDollars(){
+        /*
+        *  load current dollar amount user earned from local storage, 
+        *  and if "undefined" (i.e.,user likely didn't earn anything) then assign value to zero.
+        * 
+        *  This function is called from dynamic-survey to update the current dollar amount.
+        * 
+        */
+        
+        
+        if(window.localStorage['AwardDollar'] == undefined)
+            this.usersCurrentDollars = 0;
+        else
+            this.usersCurrentDollars = parseInt(window.localStorage['AwardDollar']);
+        
+        
+        return this.usersCurrentDollars;
+    }
+    
+    giveDollars(){
+        
+        /*
+        *  
+        * Called after survey is completed. 
+        *  
+        * Returns the total amount user has currently earned in total amount.
+        * 
+        */
+        
+        
+        var dailySurveyHistory = this.userProfileService.userProfile.survey_data.daily_survey;
+        
+        /*
+        *
+        * Scenario 1:
+        * 
+        * If user completed survey on the first day of the study then reward 2 dollars.
+        *  
+        * Since this case can't include a 3-day streak or a pause, so return this current amount
+        * 
+        */
 
-  awardDollar;
-  awardDollarObj;
-  //userProfileService: UserProfileService;;
-
-  constructor(private userProfileService: UserProfileService) { 
-    //this.userProfileService = userProfileService2;
-  }
-
-
-  getDollars(){
-      //---- load dollar amount from local storage, and if "undefined" then assign value to zero.
-      if(window.localStorage['AwardDollar'] == undefined)
-        this.awardDollar = 0;
-      else
-        this.awardDollar = parseInt(window.localStorage['AwardDollar']);
-
-      //
-      return this.awardDollar;
-  }
-
-  giveDollars(){
-    var daily_survey = this.userProfileService.userProfile.survey_data.daily_survey;
-
-    //get the first date
-    var first_date = moment().format('YYYYMMDD');
-    var first_date_moment_js = moment(first_date,"YYYYMMDD");
-    var key_moment_js;
-    for (var key in daily_survey) {
-        key_moment_js = moment(key,"YYYYMMDD");
-        //takes the first day only. But it may not be the first date.
-        if (key_moment_js < first_date_moment_js) {
-            first_date = key;
-            first_date_moment_js = moment(first_date,"YYYYMMDD");
+        //get the first date, by iterating through all dates and find the smallest one.
+        var firstDateSurveyIsCompleted = moment().format('YYYYMMDD');
+        var timestampeForFirstDataSurveyIsCompleted = moment(firstDateSurveyIsCompleted, "YYYYMMDD");
+        var timestampDateForASurveyCompleted;
+        for (var dateForASurveyCompleted in dailySurveyHistory) {
+            timestampDateForASurveyCompleted = moment(dateForASurveyCompleted,"YYYYMMDD");
+            if (timestampDateForASurveyCompleted < timestampeForFirstDataSurveyIsCompleted) {
+                firstDateSurveyIsCompleted = dateForASurveyCompleted;
+                timestampeForFirstDataSurveyIsCompleted = moment(firstDateSurveyIsCompleted,"YYYYMMDD");
+            }
         }
-    }
+        
+        // If today is the first day then award 2 dollars for survey completion; 
+        //  else load the last day, current amout user earned
+        var todaysDate = moment().format('YYYYMMDD');
+        if(todaysDate == firstDateSurveyIsCompleted){
+            this.usersCurrentDollars = 2;
+            return this.usersCurrentDollars;
+        }else
+            this.usersCurrentDollars = parseInt(window.localStorage['AwardDollar']);
+        
+        
 
-    var todays_date = moment().format('YYYYMMDD');
-    if(todays_date == first_date){
-      this.awardDollar = 2;
-    } else {
-      this.awardDollar = parseInt(window.localStorage['AwardDollar']);
-    }
 
-    // If survey is pause and start after 14 days, will get $2.
-    var onDates = this.userProfileService.userProfileFixed.onDates;
-    //set the last date to today, if onDates are not empty, will set it 
-    //to be the last date in onDates.
-    var last_date = moment().format('YYYY-MM-DD');;
+        /*
+        *
+        * Scenario 2:
+        * 
+        * If survey is paused, and user comes back after >= 14 days, then give then extra 2 dollars
+        * after they complete the survey.
+        * 
+        */
 
-    var last_date_moment_js = moment("1970-01-01", "YYYY-MM-DD")
-    var key_moment_js;
-    for (var key in onDates) {
-        key_moment_js = moment(key,"YYYY-MM-DD");
-        //takes the first day only. But it may not be the first date.
-        if (key_moment_js > last_date_moment_js) {
-          last_date = key;
-          last_date_moment_js = moment(last_date, "YYYY-MM-DD");
+
+        //
+        // Get the dates when survey  is turned on. This only contains days
+        // that the survey on button was physically clicked.
+        //  
+        var onDatesSurveyIsTurnedOn = this.userProfileService.userProfileFixed.onDates;
+        //
+        // set the last date to today, if 'onDatesSurveyPauseStarted' are not empty, will set it 
+        // to be the last date in 'onDatesSurveyPauseStarted'.
+        // 
+        var maxDateInOnDatesSurveyIsTurnedOn = moment().format('YYYY-MM-DD');;
+        
+        var timestampForLastDate = moment("1970-01-01", "YYYY-MM-DD");
+        var timestampDateFor_date;
+        for (var _date in onDatesSurveyIsTurnedOn) {
+            timestampDateFor_date = moment(_date,"YYYY-MM-DD");
+            if (timestampDateFor_date > timestampForLastDate) {
+                maxDateInOnDatesSurveyIsTurnedOn = _date;
+                timestampForLastDate = moment(maxDateInOnDatesSurveyIsTurnedOn, "YYYY-MM-DD");
+            }
         }
+        
+        var day14BeforeToday = moment().subtract(14,"days").format("YYYY-MM-DD");
+        var timestampDay14BeforeToday = moment(day14BeforeToday,"YYYY-MM-DD");
+        var timestampMaxDateInOnDatesSurveyIsTurnedOn = moment(maxDateInOnDatesSurveyIsTurnedOn,"YYYY-MM-DD");
+
+        // if 14-day before today is more than (i.e., after) timestampMaxDateInOnDatesSurveyIsTurnedOn
+        // then user has come back after 14 days.
+        if(timestampDay14BeforeToday.isAfter(timestampMaxDateInOnDatesSurveyIsTurnedOn)){
+            this.usersCurrentDollars = this.usersCurrentDollars + 2; 
+            return this.usersCurrentDollars;
+        }
+
+
+
+        /*
+        *
+        * Scenario 3:
+        * 
+        * Give money for  a three-day streak of survey completion, that does not include
+        * scenario 1 and 2. (Ideally scenario 1,2,3 are mutually exclusive).
+        * 
+        */
+
+        
+        
+        this.users3DayStreakHistory = window.localStorage["AwardDollarDates"];
+
+
+        if((this.users3DayStreakHistory == undefined) || (JSON.parse(this.users3DayStreakHistory) == null)){
+            /*
+            * Means, no history exist for 3-day, so start a streak with the current date.
+            */
+            this.users3DayStreakHistory = {};
+            this.users3DayStreakHistory['dates'] = [moment().format("DD-MM-YYYY")];      
+            window.localStorage.setItem("AwardDollarDates", JSON.stringify(this.users3DayStreakHistory));
+
+        } else { 
+            this.users3DayStreakHistory = JSON.parse(window.localStorage["AwardDollarDates"]);
+            if(this.users3DayStreakHistory['dates'].length < 2) {
+                
+                /*
+                 * see if previous date exist in the current 'users3DayStreakHistory'
+                 * If not then clean 'users3DayStreakHistory' and start a new streak.
+                 */
+                var previousDate = moment().subtract(1, "days").format("DD-MM-YYYY");
+                var dates = this.users3DayStreakHistory["dates"];
+                var dateIndex = dates.indexOf(previousDate);
+                
+                
+                if( dateIndex > -1) {
+                    //previous date exist, we will pushing the current date and extend the streak.
+                    this.users3DayStreakHistory['dates'].push(moment().format("DD-MM-YYYY"));
+                } else {
+                    // if the date saved is not the previous day of today, remove it 
+                    // save current date to users3DayStreakHistory. This means a new streak has started.
+                    this.users3DayStreakHistory['dates'] = [moment().format("DD-MM-YYYY")];          
+                }
+                window.localStorage.setItem("AwardDollarDates", JSON.stringify(this.users3DayStreakHistory));
+
+                // no money here now since the streak length it most 2 here. 
+
+            } else {
+                
+                /*
+                 * see if previous date exist in the current 'users3DayStreakHistory'
+                 * If not then clean 'users3DayStreakHistory' and start a new streak.
+                 */
+                var previousDate = moment().subtract(1, "days").format("DD-MM-YYYY");
+                var dates = this.users3DayStreakHistory["dates"];
+                var dateIndex = dates.indexOf(previousDate);
+                
+                if( dateIndex > -1 ) {
+                    //means a streak has been completed, so award one dollar, remove all dates
+                    this.usersCurrentDollars = this.usersCurrentDollars + 1;
+                    //empty users3DayStreakHistory by making it null, and we will start a new streak.
+                    window.localStorage.removeItem('AwardDollarDates'); 
+                } else {
+                    //the previous day is not in the array, means that we must have skipped the previous day,
+                    //Save current date to AwardDollarDates to start a new streak.
+                    this.usersCurrentDollars['dates'] = [moment().format("DD-MM-YYYY")];          
+                    window.localStorage.setItem("AwardDollarDates", JSON.stringify(this.usersCurrentDollars));
+                }
+                
+            }
+            
+        }    
+        
+        //save the dollar ammount in local storage
+        window.localStorage.setItem("AwardDollar", ""+this.usersCurrentDollars);    
+        
+        return this.usersCurrentDollars;
     }
-
-    console.log("last_date: "+last_date);
-    var day_14beforeToday = moment().subtract(14,"days").format("YYYY-MM-DD");
-    console.log("day_14beforeToday: "+day_14beforeToday);
-    var day_14beforeToday_moment = moment(day_14beforeToday,"YYYY-MM-DD");
-    var last_date_moment = moment(last_date,"YYYY-MM-DD");
-    if(day_14beforeToday_moment.isAfter(last_date_moment)){
-      //console.log("Pause and start: Add");
-      this.awardDollar = this.awardDollar + 2; 
-    }
-
-    //If participate submits survey continuously for 3 days, will get $1.
-    this.awardDollarObj = window.localStorage["AwardDollarDates"];
-    console.log("awardDollarObj: "+JSON.stringify(this.awardDollarObj));
-    if((this.awardDollarObj == undefined) || (JSON.parse(this.awardDollarObj) == null)){
-        this.awardDollarObj = {};
-        this.awardDollarObj['dates'] = [moment().format("DD-MM-YYYY")];      
-        window.localStorage.setItem("AwardDollarDates", JSON.stringify(this.awardDollarObj));
-    } else { 
-        this.awardDollarObj = JSON.parse(window.localStorage["AwardDollarDates"]);
-        if(this.awardDollarObj['dates'].length < 2) {
-
-          //compute previous date, and see if it exist
-          var previousdate = moment().subtract(1, "days").format("DD-MM-YYYY");
-          var dates = this.awardDollarObj["dates"];
-          var dateIndex = dates.indexOf(previousdate);
-
-          //
-          if( dateIndex > -1) {
-            //previous date exist, we will pushing the current date to the "survey-completed" dates.
-            this.awardDollarObj['dates'].push(moment().format("DD-MM-YYYY"));
-          } else {
-            // if the date saved is not the previous day of today, remove it 
-            // save current date to AwardDollarDates. This means a new streak has started.
-            this.awardDollarObj['dates'] = [moment().format("DD-MM-YYYY")];          
-          }
-          window.localStorage.setItem("AwardDollarDates", JSON.stringify(this.awardDollarObj));
-        } else {
-          //compute previous date, and check by index if it exists in dates array,
-          //the date before previous will exist as we already checked when length is 1.
-          var previousdate = moment().subtract(1, "days").format("DD-MM-YYYY");
-          var dates = this.awardDollarObj["dates"];
-          var dateIndex = dates.indexOf(previousdate);
-
-          if( dateIndex > -1 ) {
-            //means a streak has been completed, so award one dollar, remove all dates
-            this.awardDollar = this.awardDollar + 1;
-            //remove it to make it null and we will start a new streak.
-            window.localStorage.removeItem('AwardDollarDates'); 
-          } else {
-            //the previous day is not in the array, means that we must have skipped the previous day,
-            //Save current date to AwardDollarDates to count all over/
-            this.awardDollarObj['dates'] = [moment().format("DD-MM-YYYY")];          
-            window.localStorage.setItem("AwardDollarDates", JSON.stringify(this.awardDollarObj));
-          }
-
-         }
-
-    }    
-    console.log("awardDollarObj: "+JSON.stringify(this.awardDollarObj));
-
-    //save the dollar ammount in local storage
-    window.localStorage.setItem("AwardDollar", ""+this.awardDollar);    
-
-    return this.awardDollar;
-  }
 }
