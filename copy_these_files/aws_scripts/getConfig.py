dca963e2e85372f8a60dd5451bd081c4da830f40
Mash <mashfiqui.r.s@gmail.com>
2020-06-09 07:09:53 -0700

    Now tracks 4PM notification in study data check.
    Former-commit-id: f80fdd52de9ca5e3aa74798cbfd325ebd1b1b516

--- copy_these_files/aws_scripts/getConfig.py
+++ copy_these_files/aws_scripts/getConfig.py
@@ -1,7 +1,10 @@
 import json
 import io
 
-'''aws_config.json:
+'''
+
+aws_config.json:
+
 {
     "ACCESS_KEY": "......",
     "SECRET_KEY": "......"
@@ -9,9 +12,14 @@ import io
 '''
 
 # Open the JSON File and create a StringIO buffer to hold data
-with open('aws_config.json', 'r') as datafile, io.StringIO() as data:
+with open('./aws_config.json', 'r') as datafile:
+
     # Load data into json file
     config = json.load(datafile)
+    
     # Build json strong
     ACCESS_KEY = config['ACCESS_KEY']
     SECRET_KEY = config['SECRET_KEY']
+
+    # print(ACCESS_KEY)
+    # print(SECRET_KEY)
