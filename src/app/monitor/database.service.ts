9a5b7546f17b2e6ad7f57bbd3cf532f6b8b9482e
lxh37 <lxh37h@gmail.com>
2020-06-10 13:31:52 -0400

    Add back uploading SQLite to AWS.
    Former-commit-id: 1f084797c395a86806fe7559767c9ac4bfc10572

--- src/app/monitor/database.service.ts
+++ src/app/monitor/database.service.ts
@@ -84,6 +84,15 @@ export class DatabaseService {
         //this.isTableExist();
       });      
     }
+
+    emptyTable(){
+      return this.database.executeSql('DELETE FROM tracks').then(data => {
+        console.log('Table emptied!');
+      }).catch(e => {
+        console.log("deleteTable:"+JSON.stringify(e));
+      });      
+    }
+
     
     isTableExist() : Promise<any> {
       console.log("Inside isTableEmpty:");
@@ -94,6 +103,7 @@ export class DatabaseService {
         return rowlength != 0;
       }).catch(e => {
         console.log("At isTableNotEmpty:"+JSON.stringify(e));
+        return false;
       });
     }
 
@@ -105,6 +115,7 @@ export class DatabaseService {
         return rowlength == 0;
       }).catch(e => {
         console.log("At isTableNotEmpty:"+JSON.stringify(e));
+        return true;
       });
     }
 
