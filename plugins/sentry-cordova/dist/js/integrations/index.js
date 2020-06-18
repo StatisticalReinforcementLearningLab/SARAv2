b9f9fb2a7fbb0aaed9efdd0f9dbd6ac8644d69cc
Mash <mashfiqui.r.s@gmail.com>
2020-06-18 11:01:49 -0700

    Some money fix.
    Added Sentry for bug tracking.
    Fish awarded on the first day.
    Money is tracked on the first day or when people comeback.
    
    
    Former-commit-id: 4f165a5c29d8799f5dc6122517983610b5922b7e

---
+++ plugins/sentry-cordova/dist/js/integrations/index.js
@@ -0,0 +1,6 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+var release_1 = require("./release");
+exports.Release = release_1.Release;
+var cordova_1 = require("./cordova");
+exports.Cordova = cordova_1.Cordova;
+//# sourceMappingURL=index.js.map
---
+++ plugins/sentry-cordova/src/ios/SentryCordova.m
@@ -0,0 +1,128 @@
+#import "SentryCordova.h"
+#import <Cordova/CDVAvailability.h>
+@import Sentry;
+
+NSString *const SentryCordovaVersionString = @"0.17.0";
+NSString *const SentryCordovaSdkName = @"sentry-cordova";
+
+@implementation SentryCordova
+
+- (void)pluginInitialize {
+    NSLog(@"Sentry Cordova Plugin initialized");
+}
+
+- (void)install:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        NSString *dsn = [command.arguments objectAtIndex:0];
+        NSError *error = nil;
+        SentryClient *client = [[SentryClient alloc] initWithDsn:dsn didFailWithError:&error];
+        SentryClient.sharedClient = client;
+        [SentryClient.sharedClient startCrashHandlerWithError:&error];
+        client.beforeSerializeEvent = ^(SentryEvent * _Nonnull event) {
+            [self setReleaseVersionDist:event];
+        };
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
+        if (error != nil) {
+            NSLog(@"%@", error);
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:NO];
+        }
+        [SentryClient.sharedClient enableAutomaticBreadcrumbTracking];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    }];
+}
+
+- (void)setReleaseVersionDist:(SentryEvent *)event {
+    if (event.extra[@"__sentry_version"]) {
+        NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
+        event.releaseName = [NSString stringWithFormat:@"%@-%@", infoDict[@"CFBundleIdentifier"], event.extra[@"__sentry_version"]];
+    }
+    if (event.extra[@"__sentry_release"]) {
+        event.releaseName = [NSString stringWithFormat:@"%@", event.extra[@"__sentry_release"]];
+    }
+    if (event.extra[@"__sentry_dist"]) {
+        event.dist = [NSString stringWithFormat:@"%@", event.extra[@"__sentry_dist"]];
+    }
+    event.sdk = @{@"name": SentryCordovaSdkName,
+                  @"version": SentryCordovaVersionString,
+                  @"integrations": @[@"sentry-cocoa"]};
+}
+
+- (void)sendEvent:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        NSDictionary *jsonEvent = [command.arguments objectAtIndex:0];
+        if ([NSJSONSerialization isValidJSONObject:jsonEvent]) {
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonEvent
+                                                               options:0
+                                                                 error:nil];
+
+            SentryEvent *sentryEvent = [[SentryEvent alloc] initWithJSON:jsonData];
+            if (SentryClient.sharedClient != nil) {
+                [SentryClient.sharedClient sendEvent:sentryEvent withCompletionHandler:^(NSError * _Nullable error) {
+                    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
+                    if (error != nil) {
+                        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:NO];
+                    }
+                    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                }];
+            }
+        }
+    }];
+}
+
+- (void)addBreadcrumb:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        NSDictionary *jsonBreadcrumb = [command.arguments objectAtIndex:0];
+        if ([jsonBreadcrumb isKindOfClass:NSDictionary.class]) {
+          SentryBreadcrumb *breadcrumb = [SentryJavaScriptBridgeHelper createSentryBreadcrumbFromJavaScriptBreadcrumb:jsonBreadcrumb];
+          [SentryClient.sharedClient.breadcrumbs addBreadcrumb:breadcrumb];
+          CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:[breadcrumb serialize]];
+          [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        }
+    }];
+}
+
+
+- (void)setUserContext:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        id arg = [command.arguments objectAtIndex:0];
+        if ([arg isKindOfClass:NSDictionary.class]) {
+            SentryClient.sharedClient.user = [SentryJavaScriptBridgeHelper createSentryUserFromJavaScriptUser:arg];
+        }
+    }];
+}
+
+- (void)setTagsContext:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        if (SentryClient.sharedClient && SentryClient.sharedClient.tags) {
+            id dict = [command.arguments objectAtIndex:0];
+            if ([dict isKindOfClass:NSDictionary.class]) {
+                NSMutableDictionary *newDict = [NSMutableDictionary new];
+                [newDict addEntriesFromDictionary:SentryClient.sharedClient.tags];
+                [newDict addEntriesFromDictionary:dict];
+                SentryClient.sharedClient.tags = newDict;
+            }
+        }
+    }];
+}
+
+- (void)setExtraContext:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        if (SentryClient.sharedClient && SentryClient.sharedClient.extra) {
+            id dict = [command.arguments objectAtIndex:0];
+            if ([dict isKindOfClass:NSDictionary.class]) {
+                NSMutableDictionary *newDict = [NSMutableDictionary new];
+                [newDict addEntriesFromDictionary:SentryClient.sharedClient.extra];
+                [newDict addEntriesFromDictionary:dict];
+                SentryClient.sharedClient.extra = newDict;
+            }
+        }
+    }];
+}
+
+- (void)clearContext:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        [SentryClient.sharedClient clearContext];
+    }];
+}
+
+@end
