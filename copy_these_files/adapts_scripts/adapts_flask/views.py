db3452013372ec43ee5f87fdbe1e5d5c43cc98c1
Mash <mashfiqui.r.s@gmail.com>
2020-06-18 11:01:49 -0700

    Some money fix.
    Added Sentry for bug tracking.
    Fish awarded on the first day.
    Money is tracked on the first day or when people comeback.
    
    
    Former-commit-id: 8ce97f877be8df408375cde141847a8907185c02

---
+++ copy_these_files/adapts_scripts/adapts_flask/views.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+from run import app
+from flask import jsonify
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello, World!'})
---
+++ plugins/sentry-cordova/dist/js/version.js
@@ -0,0 +1,4 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SDK_NAME = 'sentry.javascript.cordova';
+exports.SDK_VERSION = '0.17.0';
+//# sourceMappingURL=version.js.map
