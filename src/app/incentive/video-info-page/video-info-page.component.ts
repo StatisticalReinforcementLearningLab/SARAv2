6ff345db8314b75a2d35d636a2bd61e473680aab
Mash <mashfiqui.r.s@gmail.com>
2020-07-26 15:32:35 -0700

    - Added a simple video tutorial page. - Fixed the "Fishes" to "Fish"
    Former-commit-id: be2f16144748afa27dc3eb462ca5efe6185d9c7e

---
+++ src/app/incentive/video-info-page/video-info-page.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { DatabaseService } from 'src/app/monitor/database.service';
+
+@Component({
+  selector: 'app-video-info-page',
+  templateUrl: './video-info-page.component.html',
+  styleUrls: ['./video-info-page.component.css']
+})
+export class VideoInfoPageComponent implements OnInit {
+
+  constructor(private menuCtrl:MenuController,
+    private appUsageDb: DatabaseService) { }
+
+  ngOnInit() {
+    this.menuCtrl.close();
+  }
+
+  ionViewDidEnter(){
+    /*
+    this.db.getDatabaseState().subscribe(rdy => {
+     if (rdy) {     
+       this.db.addTrack(this.pageTitle, "Enter", this.userProfileService.username, Object.keys(this.userProfileService.userProfile.survey_data.daily_survey).length); 
+     }
+    });
+    */
+    this.appUsageDb.saveAppUsageEnter("reward_video_info_page");  
+  }  
+
+  ionViewDidLeave(){
+    /*
+    this.db.getDatabaseState().subscribe(rdy => {
+      if (rdy) {     
+        this.db.addTrack(this.pageTitle, "Leave", this.userProfileService.username, Object.keys(this.userProfileService.userProfile.survey_data.daily_survey).length); 
+      }
+    });
+    */ 
+    this.appUsageDb.saveAppUsageExit("reward_video_info_page");     
+  }
+
+}
