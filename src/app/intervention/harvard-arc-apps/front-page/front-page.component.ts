5352d414067d1ea72221e6f9f211d5071c911cc6
Andrew Courtney <acourtney7723@gmail.com>
2020-07-27 22:03:16 -0700

    added arc apps through json file

--- src/app/intervention/harvard-arc-apps/front-page/front-page.component.ts
+++ src/app/intervention/harvard-arc-apps/front-page/front-page.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController, NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
-
+import * as data from './arc_apps.json';
 
 @Component({
   selector: 'app-front-page',
   templateUrl: './front-page.component.html',
   styleUrls: ['./front-page.component.css']
+
 })
 export class FrontPageComponent implements OnInit {
+
+  products: any = (data as any).default;
 
   constructor(private router: Router, 
     public navController: NavController,
