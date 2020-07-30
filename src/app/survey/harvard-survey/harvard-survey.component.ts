d4ea63117f9c60d0fde84c94160a5ba1c5ba5e37
Mash <mashfiqui.r.s@gmail.com>
2020-07-30 10:00:52 -0700

    -  sourcemap deactivated - Changed accessibility settings for harvard survey page - aquarium page is using. 'navController'
    Former-commit-id: 23e712a839e5f190d45bc39a67d084c649c39694

--- src/app/survey/harvard-survey/harvard-survey.component.ts
+++ src/app/survey/harvard-survey/harvard-survey.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserProfileService } from 'src/app/user/user-profile/user-profile.service';
+import { MobileAccessibility } from '@ionic-native/mobile-accessibility/ngx';
+import { DatabaseService } from 'src/app/monitor/database.service';
 
 @Component({
   selector: 'app-harvard-survey',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HarvardSurveyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userProfileService: UserProfileService,
+    private mobileAccessibility: MobileAccessibility,
+    private appUsageDb: DatabaseService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+      this.mobileAccessibility.usePreferredTextZoom(false);
+    }
+  
+    ionViewDidEnter(){
+      /*
+      this.db.getDatabaseState().subscribe(rdy => {
+       if (rdy) {     
+         this.db.addTrack(this.pageTitle, "Enter", this.userProfileService.username, Object.keys(this.userProfileService.userProfile.survey_data.daily_survey).length); 
+       }
+      });
+      */
+      this.appUsageDb.saveAppUsageEnter("harvard_survey_page");
+   }  
+  
+   ionViewDidLeave(){
+      /*
+      console.log(this.pageTitle+": ionViewDidLeave");
+      this.db.getDatabaseState().subscribe(rdy => {
+        if (rdy) {     
+          this.db.addTrack(this.pageTitle, "Leave", this.userProfileService.username, Object.keys(this.userProfileService.userProfile.survey_data.daily_survey).length); 
+        }
+      });
+      */    
+      this.mobileAccessibility.usePreferredTextZoom(true);
+      this.appUsageDb.saveAppUsageExit("harvard__survey_page");
+    
+   }
 
 }
