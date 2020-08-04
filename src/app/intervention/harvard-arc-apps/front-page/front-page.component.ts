002c45b701a78e523ede6601977ce336aff126b5
Frank Zhu <43142527+zhuf11@users.noreply.github.com>
2020-08-03 20:09:03 -0400

    Updated with more apps
    Former-commit-id: 14e072338c0e16f07139995a9ae92bb618a04e54

--- src/app/intervention/harvard-arc-apps/front-page/front-page.component.ts
+++ src/app/intervention/harvard-arc-apps/front-page/front-page.component.ts
@@ -29,12 +29,63 @@ export class FrontPageComponent implements OnInit {
             img: "./assets/img/arcapps/tomatoapp.png",
             description: "Tomato timer chunks larger-works into a set of intervals separated by short breaks."
         },
+
         {
             name: "Forest",
             url: "https://www.forestapp.cc/",
             img: "./assets/img/arcapps/forestapp.png",
             description: "Forest is an app that helps you stay focused on the important things in life."
-        }
+        },
+
+        {
+            name: "Focus",
+            url: "https://heyfocus.com/",
+            img: "./assets/img/arcapps/focusapp.png",
+            description: "Focus is a distraction blocker that helps you improve your productivity."
+        },
+
+        {
+            name: "MyStudyLife",
+            url: "https://www.mystudylife.com/",
+            img: "./assets/img/arcapps/mystudylifeapp.png",
+            description: "MyStudyLife is a cross-platform planner that stores classes, homework, and exams in the cloud."
+        },
+
+        {
+            name: "Week Plan",
+            url: "https://weekplan.net/",
+            img: "./assets/img/arcapps/weekplanapp.png",
+            description: "Week Plan is a priority planner for highly effective people."
+        },
+
+        {
+            name: "SelfControl",
+            url: "https://selfcontrolapp.com/",
+            img: "./assets/img/arcapps/selfcontrolapp.png",
+            description: "SelfControl is a free Mac application to help you avoid distracting websites."
+        },
+
+        {
+            name: "Freedom",
+            url: "https://freedom.to/",
+            img: "./assets/img/arcapps/freedomapp.jpg",
+            description: "Freedom is an app and website blocker used by over 1,000,000 people to reclaim focus and productivity."
+        },
+
+        {
+           name: "StayFocusd",
+            url: "https://chrome.google.com/webstore/detail/stayfocusd/laankejkbhbdhmipfmgcngdelahlfoji?hl=en",
+            img: "./assets/img/arcapps/stayfocusdapp.png",
+            description: "StayFocusd is a productivity extension for Google Chrome that restricts the amount of time you can spend on time-wasting websites."
+        },
+
+        {
+           name: "Cold Turkey",
+            url: "https://getcoldturkey.com/",
+            img: "./assets/img/arcapps/coldturkeyapp.png",
+            description: "Cold Turkey is a free website blocker designed for studying or focusing on work."
+        },
+
     ];
 
     TM_apps: ArcApp[] = [
@@ -43,7 +94,6 @@ export class FrontPageComponent implements OnInit {
         { name: "Focus", url: "https://heyfocus.com/" },
         { name: "mystudylife", url: "https://tomato-timer.com/" },
         { name: "weekplan", url: "https://tomato-timer.com/" },
-        { name: "Pomodoro Technique", url: "https://tomato-timer.com/" },
     ];
 
     Internet_Blocking_Tools: ArcApp[] = [
