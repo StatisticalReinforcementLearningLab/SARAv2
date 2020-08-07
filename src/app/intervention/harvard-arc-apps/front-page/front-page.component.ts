89d6405f53cbfca87a5ebdcff808c69d0b799af2
Andrew Courtney <acourtney7723@gmail.com>
2020-08-06 23:04:07 -0700

    Done fixing up json file

--- src/app/intervention/harvard-arc-apps/front-page/front-page.component.ts
+++ src/app/intervention/harvard-arc-apps/front-page/front-page.component.ts
@@ -18,7 +18,6 @@ export class FrontPageComponent implements OnInit {
 
   ngOnInit() {
     this.menuCtrl.close();
-    
   }
 
   ngAfterViewInit() {
@@ -29,12 +28,18 @@ export class FrontPageComponent implements OnInit {
     this.navController.navigateRoot(['home']);
   }
 
-fetchARCApps() {
-  let app_list = [];
-  fetch('../../../assets/data/' + this.jsonFileLinkForSurvey + '.json').then(async res => {
-    this.app_list = await res.json();
-    return app_list;
-  })
-  .then(data => console.log(data));
+  fetchARCApps() {
+    let app_list = [];
+    fetch('../../../assets/data/' + this.jsonFileLinkForSurvey + '.json').then(async res => {
+      this.app_list = await res.json();
+      return app_list;
+    })
+    .then(data => console.log(data));
+  }
+
+  visitTheURL(url){
+    console.log("visitTheURL");
+    window.open(url, '_system', 'location=yes'); 
+    return false;
   }
 }
