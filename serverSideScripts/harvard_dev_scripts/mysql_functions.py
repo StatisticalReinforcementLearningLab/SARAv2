865c566ea02f9d6a48dac18473166c0f66218dda
Chloe Loughridge <cloughridge@college.harvard.edu>
2020-08-25 16:22:53 -1000

    mysql code refactored

--- serverSideScripts/harvard_dev_scripts/mysql_functions.py
+++ serverSideScripts/harvard_dev_scripts/mysql_functions.py
@@ -3,9 +3,35 @@ import mysql.connector as mysql
 from datetime import datetime
 import time
 import json
-from getConfig_mysql import DB_PASSWORD
+from getConfig_mysql import DB_PASSWORD, DB_HOST, DB_PORT, DB_USER
 
 # it would be a good idea to download mysql workbench
+
+def connect_to_database(db_name):
+    """
+    Connects to sql database. Returns a db object.
+    """
+    return mysql.connect(
+        host = DB_HOST,
+        port = DB_PORT,
+        user = DB_USER,
+        passwd = DB_PASSWORD,
+        database = db_name
+    )
+
+def config_harvardSurvey_database():
+    """
+    Ensures all columns of the harvardSurvey database are set to the appropriate type.
+    """
+    db = connect_to_database("HarvardDev")
+    cursor = db.cursor()
+
+    cursor.execute("ALTER TABLE harvardSurvey MODIFY COLUMN survey_completion_time VARCHAR (20);")
+    cursor.execute("ALTER TABLE harvardSurvey MODIFY COLUMN json_answer TEXT (100000);")
+    cursor.execute("ALTER TABLE harvardSurvey ADD COLUMN when_inserted VARCHAR (50);")
+    # add a column to record time inserted
+
+    db.commit()
 
 def insert_data_into_mysql(payload):
     """
@@ -14,164 +40,99 @@ def insert_data_into_mysql(payload):
     to all of the questions
 
     """
-
     print('Inserting data')
 
     # connect to db
-    db = mysql.connect(
-        host = "ec2-54-91-131-166.compute-1.amazonaws.com",
-        port = 3308,
-        user = "root",
-        passwd = DB_PASSWORD,
-        database = "HarvardDev"
-    )
+    db = connect_to_database("HarvardDev")
     cursor = db.cursor()
 
-    # insert data
-    # whenIntertedTs = time.time()
-    # whenInsertedReadableTs = datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    #payload = "temp data"
-
-    #json_data = json.loads(payload)
+    # removing keys and inserting separately into table
     userID = payload.pop('userName')
     completionTime = str(payload.pop('endtimeUTC'))
-
-    # FIXME: make this a separate function; it only requires one-time use, right?
-    cursor.execute("ALTER TABLE harvardSurvey MODIFY COLUMN survey_completion_time VARCHAR (20);")
-    cursor.execute("ALTER TABLE harvardSurvey MODIFY COLUMN json_answer TEXT (100000);")
+    whenIntertedTs = time.time()
+    whenInsertedReadableTs = datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
     insert_stmt = (
-      "INSERT INTO harvardSurvey (user_id, survey_completion_time, json_answer) "
-      "VALUES (%s, %s, %s)"
+      "INSERT INTO harvardSurvey (user_id, survey_completion_time, json_answer, when_inserted) "
+      "VALUES (%s, %s, %s, %s)"
     )
-    data = (userID, completionTime, json.dumps(payload))
+    data = (userID, completionTime, json.dumps(payload), whenInsertedReadableTs)
     cursor.execute(insert_stmt, data)
     db.commit()
 
-def select_data_from_mysql():
+def select_all_data():
     """
     Select all data points from the test database.
     """
 
-    print('Fetching data')
+    #print('Fetching data')
 
     # connect to db
-    db = mysql.connect(
-        host = "ec2-54-91-131-166.compute-1.amazonaws.com",
-        port = 3308,
-        user = "root",
-        passwd = DB_PASSWORD,
-        database = "HarvardDev"
-    )
+    db = connect_to_database("HarvardDev")
     cursor = db.cursor()
 
     # fetch data.
-    cursor.execute("SELECT * FROM harvardSurvey")
-    recordsInTestTable = cursor.fetchall()
+    cursor.execute("SELECT user_id, survey_completion_time, json_answer, when_inserted FROM harvardSurvey")
+    return cursor.fetchall()
 
-    # print data.
-    for row in recordsInTestTable:
-        print(row)
-        print("\n")
-
-def select_questions_from_mysql(n):
+def get_recent_time(n):
     """
-    Select the question data based on provided username.
+    Get the time of the most recent survey_completion_time for a given user_id.
+    - n: string, user_id
+    """
+    db = connect_to_database("HarvardDev")
+    cursor = db.cursor()
+    cursor.execute("SELECT MAX(survey_completion_time) FROM harvardSurvey WHERE user_id = {}".format("'"+n+"'"))
+    return cursor.fetchall()[0][0]
+
+def get_question_data(n):
+    """
+    Select the most recent question data based on provided username. Returns that
+    question data in json form.
     - n: string, username
     """
-
-    print('Fetching data')
-
-    # connect to db
-    db = mysql.connect(
-        host = "ec2-54-91-131-166.compute-1.amazonaws.com",
-        port = 3308,
-        user = "root",
-        passwd = DB_PASSWORD,
-        database = "HarvardDev"
-    )
+    db = connect_to_database("HarvardDev")
     cursor = db.cursor()
+    recentTime = get_recent_time(n)
+    cursor.execute("SELECT json_answer FROM harvardSurvey WHERE user_id = {} AND survey_completion_time = {}"\
+        .format("'"+n+"'", recentTime))
 
-    # fetch data.
-    cursor.execute("SELECT * FROM harvardSurvey")
-    recordsInTestTable = cursor.fetchall()
+    try:
+        questionData = json.loads(cursor.fetchall()[0][0])
+    except:
+        raise Exception("Could not find most recent question data for specified user.")
 
-    counter = 0
-    old_time = 0
-    recent_answer = ''
-
-    # eventually relevant data should be in string form in the table
-
-    # FIXME!! STRUCTURE OF DATABASE HAS CHANGED
-    #print data.
-    for row in recordsInTestTable:
-
-        row_dict = json.loads(row[3])
-
-        # FIXME: too many magic numbers
-        #if row_dict["userName"] == n:
-        if row[1] == n:
-            answer = row_dict["Q4"]
-            #time = row_dict["endtimeUTC"]
-            time = row[2]
-            print("Answer to question 4 from {} at time {}: {}".format(n, time, answer))
-            if int(time) > old_time:
-                recent_answer = str(answer)
-                old_time = int(time)
-
-    return recent_answer
-
+    if "Q4" in questionData:
+        return questionData
+    else:
+        pass
 
 def get_usernames():
     """
     Returns a list of unique usernames from the sql database.
     """
-    # FIXME: NEW DATABASE
-    # connect to db
-    db = mysql.connect(
-        host = "ec2-54-91-131-166.compute-1.amazonaws.com",
-        port = 3308,
-        user = "root",
-        passwd = DB_PASSWORD,
-        database = "HarvardDev"
-    )
+    db = connect_to_database("HarvardDev")
     cursor = db.cursor()
-
-    # fetch data.
-    # when SQL structure changes, use SELECT DISTINCT
-    cursor.execute("SELECT * FROM testTable")
-    recordsInTestTable = cursor.fetchall()
-
-    usernames = {}
-
-    for row in recordsInTestTable:
-
-        row_dict = json.loads(row[3])
-        usernames[row_dict["userName"]] = row_dict["userName"]
-
-    return usernames.keys()
+    cursor.execute("SELECT DISTINCT user_id FROM harvardSurvey")
+    user_ids = cursor.fetchall()
+    id_lst = []
+    for name, in user_ids:
+        id_lst.append(name)
+    return id_lst
 
 def clear_all_sql():
     """
     Delete all records in the sql database.
+    NOTE: currently depends on magic number for maximum number of records in
+    table.
     """
-    print('Clearing all records.')
+    print('Clearing all records from harvardSurvey table.')
 
     # connect to db
-    db = mysql.connect(
-        host = "ec2-54-91-131-166.compute-1.amazonaws.com",
-        port = 3308,
-        user = "root",
-        passwd = DB_PASSWORD,
-        database = "HarvardDev"
-    )
+    db = connect_to_database("HarvardDev")
     cursor = db.cursor()
 
-    # NOT WORKING --> maybe ask mash how to do this
-
-    #DELETE * FROM testTable
-
-    cursor.execute("DELETE FROM harvardSurvey WHERE id BETWEEN 0 AND 200")
+    cursor.execute("DELETE FROM harvardSurvey WHERE id BETWEEN 0 AND 10000")
     db.commit()
 
 def get_player_id(username):
@@ -179,49 +140,20 @@ def get_player_id(username):
     Returns most recent player id based on username.
     """
     # connect to db
-    db = mysql.connect(
-        host = "ec2-54-91-131-166.compute-1.amazonaws.com",
-        port = 3308,
-        user = "root",
-        passwd = DB_PASSWORD,
-        database = "SARAA"
-    )
+    db = connect_to_database("SARAApp")
     cursor = db.cursor()
     cursor.execute("SELECT oneSignalPlayerId FROM SARAApp.user_ids WHERE user_id = {} ORDER BY currentTimeTs DESC LIMIT 1"\
         .format("'"+ username+"'"))
     pid = cursor.fetchall()[0][0]
 
     return pid
-# def clear_by_username(name):
-#     """
-#     Delete records based on username.
-#     """
-#     print("Clearning records with username: {}".format(name))
-
-#     # connect to db
-#     db = mysql.connect(
-#         host = "ec2-54-91-131-166.compute-1.amazonaws.com",
-#         port = 3308,
-#         user = "root",
-#         passwd = "helloworld",
-#         database = "HarvardDev"
-#     )
-#     cursor = db.cursor()
-
-#     cursor.execute("DELETE * FROM testTable WHERE id BETWEEN 0 AND 200")
-#     db.commit()
-
 
 ## Testing
-# #insert_data_into_mysql()
-# print("Showing what is in the database")
-# select_data_from_mysql()
-# print("Clearing the database")
-# clear_all_sql()
-# select_data_from_mysql()
+if __name__ == '__main__':
+    #config_harvardSurvey_database()
+    #print(get_player_id("susan_aya"))
+    #get_question_data("mash_aya")
+    get_usernames()
 
-#select_questions_from_mysql("chloe_aya")
-# test = get_player_id("mash_aya")
-# print(test)
 
 
