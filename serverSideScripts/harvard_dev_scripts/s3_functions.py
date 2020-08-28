430cd239ea4e2eeaf85781f6f4b8e0422da8b1ef
Chloe Loughridge <cloughridge@college.harvard.edu>
2020-08-27 15:36:11 -1000

    processed data moved to different directory

--- serverSideScripts/harvard_dev_scripts/s3_functions.py
+++ serverSideScripts/harvard_dev_scripts/s3_functions.py
@@ -23,18 +23,67 @@ from mysql_functions import (insert_data_into_mysql, select_all_data,
 #Look at getConfig.py to create config Json file.
 from getConfig_aws import AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION_NAME
 
-def transfer_s3_data(directory, bucket_name):
+def create_boto_client():
+    """
+    Returns a boto3 client created using the credentials above.
+    """
+    return boto3.client("s3", region_name = AWS_REGION_NAME,
+                               aws_access_key_id=AWS_ACCESS_KEY,
+                               aws_secret_access_key=AWS_SECRET_KEY)
+
+def create_boto_resource():
+    """
+    Returns a boto3 resource created using the credentials above.
+    """
+    return boto3.resource("s3", region_name = AWS_REGION_NAME,
+                               aws_access_key_id=AWS_ACCESS_KEY,
+                               aws_secret_access_key=AWS_SECRET_KEY)
+
+def move_datapoint(bucket_name, source_directory, dest_directory, filename):
+    """
+    Move a datapoint from source_directory to dest_directory in AWS s3 bucket. Deletes
+    the data in the original directory.
+    - bucket_name: string, name of AWS s3 bucket we are operating within
+    - source_directory: string, folder name where surveys are originally stored
+    - dest_directory: string, folder name to which to transfer the survey data
+    - filename: string, name of survey data entry
+    """
+    s3 = create_boto_resource()
+    print("Moving {} from {} to {}.".format(filename, source_directory, dest_directory))
+    copy_source = {
+        "Bucket": bucket_name,
+        'Key': source_directory + filename
+    }
+    s3.Object(bucket_name, dest_directory + filename).copy_from(CopySource=copy_source)
+    s3.Object(bucket_name, source_directory + filename).delete()
+
+def move_all_data(bucket_name, source_directory, dest_directory):
+    """
+    Moves all of the data from source_directory into dest_directory.
+    within the AWS s3 bucket.
+    - bucket_name: string, name of AWS s3 bucket we are operating within
+    - source_directory: string, folder name where surveys are originally stored
+    - dest_directory: string, folder name to which to transfer the survey data
+    """
+    print("Moving all contents of {} to {}.".format(source_directory, dest_directory))
+    client = create_boto_client()
+    # read a list of objects (i.e., filenames) from S3
+    resp = client.list_objects_v2(Bucket=bucket_name,Prefix=source_directory)
+
+    for obj in resp['Contents']:
+        filename = obj['Key'].split("/")[-1]
+        move_datapoint(bucket_name, source_directory, dest_directory, filename)
+
+def transfer_s3_data(bucket_name, directory, processed_directory):
     """
     Transfers survey data from s3 survey collection bucket to sql table.
     Next stage: call this function every 15 minutes
+    - directory: string, name of folder where survey data uploaded
+    - bucket_name: string, name of amazon s3 bucket
+    - processed_directory: string, name of folder in which to store processed survey data
     """
     #Use Client to access s3 
-    client = boto3.client("s3", region_name = AWS_REGION_NAME,
-                               aws_access_key_id=ACCESS_KEY,
-                               aws_secret_access_key=SECRET_KEY)
-
-    #bucket_name = 'sara-dev-data-storage' #Change this for a different project
-    #s3_directory_location_with_surveys = 'harvard_survey/' #Change this for a different project
+    client = create_boto_client()
 
     # read a list of objects (i.e., filenames) from S3
     resp = client.list_objects_v2(Bucket=bucket_name,Prefix=directory)
@@ -68,17 +117,23 @@ def transfer_s3_data(directory, bucket_name):
 
                 insert_data_into_mysql(clean_data)
 
-def process_data():
-    """
-    Move data from response S3 bucket to process bucket.
-    """
-    pass
+                # move the object into the processed folder
+                # and delete from the survey folder
+                # later we may want to create subfolders in processed directory
+                # so that we can have backups from different timepoints
+                name = filename.split("/")[-1]
+                move_datapoint(bucket_name, directory, 'harvard_survey_processed/', name)
+
 
 # Testing
 if __name__ == '__main__':
     #clear_all_sql()
-    transfer_s3_data('harvard_survey/', 'sara-dev-data-storage')
+    bucketName = 'sara-dev-data-storage'
+    surveyDirectory = 'harvard_survey/'
+    processedDirectory = 'harvard_survey_processed/'
+    transfer_s3_data(bucketName, surveyDirectory, processedDirectory)
     get_question_data("mash_aya")
+    move_all_data(bucketName, processedDirectory, surveyDirectory)
     #one_signal()
 
 # make sure we've inserted everything properly into the sql database
