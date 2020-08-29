a3415f134859392513ecdd78d29ebf03a132635e
Chloe Loughridge <cloughridge@college.harvard.edu>
2020-08-28 17:06:18 -1000

    the oneSignal script has been objectified

--- serverSideScripts/harvard_dev_scripts/cronjob_script.py
+++ serverSideScripts/harvard_dev_scripts/cronjob_script.py
@@ -1,5 +1,5 @@
 from s3_functions import transfer_s3_data
-from onesignal_functions import createOneSignalMessage, sendOneSignalNotifications
+from onesignal_functions import OneSignal
 from mysql_functions import get_question_data, get_usernames, get_player_id
 
 
@@ -21,10 +21,10 @@ def main():
 		if playerID is not None:
 			# also get the survey data
 			surveyResponses = get_question_data(userName)
-			# create a OneSignal message
-			msg = createOneSignalMessage(surveyResponses["Q4"])
-			# send the OneSignal message
-			sendOneSignalNotifications([playerID], msg)
+			
+			msg = OneSignal(playerID, surveyResponses["Q4"], timeToSend="6:00 AM",\
+				msgHeading="Harvard test title", notificationImage='fishjournal.png')
+			msg.send()
 
 	print("Finished sending notifications to all users who completed surveys.")
 
