17072ec4997bd56b923016770fe385d95060c8a0
Mash <mashfiqui.r.s@gmail.com>
2020-11-17 17:57:33 -0800

    Early pipeline. codes

---
+++ server_side_scripts/back_end_pipeline/visualization/SingleVisualizationInterface.py
@@ -0,0 +1,26 @@
+#
+# Created by Mash, Nov 16, 2020
+#
+
+
+class SingleVisualizationInterface:
+
+    def __init__(self, name, description):
+        self.name = name # give a name to this visualization
+        self.description = description # give a description to this visualization
+    
+    def get_visualization_description(self):
+        """
+        return an object with a description of the visualization.
+        """
+        pass
+
+    def generate_time_series_for_plot(self, arguments):
+        pass
+
+    def store_visualization_to_s3(self):
+        pass
+
+    def store_time_series_to_s3(self):
+        pass
+
---
+++ server_side_scripts/back_end_pipeline/visualization/VisualizationFactory.py
@@ -0,0 +1,24 @@
+class VisualizationFactory:
+    def __init__(self):
+        self.list_of_visualizations = []
+
+
+    def generate_and_save_chart(self, chart_name):
+        """
+        generate and save charts
+        """
+        pass
+
+    def generate_and_save_all_charts(self):
+        """
+        generate and save all charts
+        """
+        pass
+
+
+    def return_description_existing_charts(self):
+        """
+        return a list of all charts
+        """
+        pass
+
