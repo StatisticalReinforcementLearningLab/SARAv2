ea0eead5e574e111510cf165b1103ca644794f2c
Mash <mashfiqui.r.s@gmail.com>
2021-02-26 07:06:54 -0800

    Individualize survey, reinforcement data processing is added. This doesn't have the 1000 file limit

---
+++ data_analysis_scripts/getConfig.py
@@ -0,0 +1,25 @@
+import json
+import io
+
+
+'''aws_config.json:
+{
+    "ACCESS_KEY": "......",
+    "SECRET_KEY": "......"
+}
+'''
+
+# Open the JSON File and create a StringIO buffer to hold data
+
+with open('./aws_config_CHOP.json', 'r') as datafile:
+
+    # Load data into json file
+    config = json.load(datafile)
+    
+    # Build json strong
+    ACCESS_KEY = config['ACCESS_KEY']
+    SECRET_KEY = config['SECRET_KEY']
+
+    # print(ACCESS_KEY)
+    # print(SECRET_KEY)
+
