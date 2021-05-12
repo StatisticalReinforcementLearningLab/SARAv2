c8b1f24cc6ec7ae0394f22e8f1f6c695e0328541
sarahrathnam <sarah.rathnam@gmail.com>
2021-05-12 13:05:04 -0400

    new dogs survey

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,11 +26,12 @@ import { FishbowlComponent } from './incentive/aquarium/previews/fishbowl/fishbo
 import { SeaComponent } from './incentive/aquarium/previews/sea/sea.component';
 import { TundraComponent } from './incentive/aquarium/previews/tundra/tundra.component';
 import { RainforestComponent } from './incentive/aquarium/previews/rainforest/rainforest.component';
-import { HarvardSurveyComponent } from './survey/harvard-survey/harvard-survey.component';
+//import { HarvardSurveyComponent } from './survey/harvard-survey/harvard-survey.component';
 import { FrontPageComponent } from './intervention/harvard-arc-apps/front-page/front-page.component';
 import { VideoInfoPageComponent } from './incentive/video-info-page/video-info-page.component';
 import { SamplePythonViewComponent } from './incentive/life-insights/sample-python-view/sample-python-view.component';
 import { SleepSurveyComponent } from './survey/sleep-survey/sleep-survey.component';
+import { DogsSurveyComponent } from './survey/dogs-survey/dogs-survey.component';
 
 
 const routes: Routes = [
@@ -59,8 +60,9 @@ const routes: Routes = [
   { path: 'incentive/cheatpoints', component: CheatpageComponent, canActivate: [AuthGuard]},
   { path: 'survey/samplesurvey', component: SampleSurveyComponent, canActivate: [AuthGuard]}, 
   { path: 'survey/samplesurvey2', component: AyaSampleSurveyComponent, canActivate: [AuthGuard]}, 
-  { path: 'survey/harvardsurvey', component: HarvardSurveyComponent, canActivate: [AuthGuard]}, 
+  //{ path: 'survey/harvardsurvey', component: HarvardSurveyComponent, canActivate: [AuthGuard]}, 
   { path: 'survey/sleepsurvey', component: SleepSurveyComponent, canActivate: [AuthGuard]},
+  { path: 'survey/dogssurvey', component: DogsSurveyComponent, canActivate: [AuthGuard]},
   { path: 'intervention/arcappsfrontpage', component: FrontPageComponent, canActivate: [AuthGuard]}, 
   { path: 'incentive/aquariumone', component: DemoAquariumComponent, canActivate: [AuthGuard]},
   { path: 'incentive/award-memes', component: AwardMemesComponent, canActivate: [AuthGuard]},
