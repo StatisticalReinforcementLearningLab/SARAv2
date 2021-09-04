e7aefcff69491b6e08d3c164edab35653a540c89
Mash <mashfiqui.r.s@gmail.com>
2021-09-04 16:18:58 -0700

    Added sleep monitoring as a tab. The app data is sent to server using a public private key encryption. The environment file contains the public key. The environment file will distributed over slack.
    Former-commit-id: 0234ad6e0c2f764ddbb5b37e7e0f327aa92e592d

--- src/app/intervention/intervention.module.ts
+++ src/app/intervention/intervention.module.ts
@@ -1,15 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HarvardArcAppsModule } from './harvard-arc-apps/harvard-arc-apps.module';
+import { SleepMonitoringComponent } from './sleep-monitoring/sleep-monitoring.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SleepMonitoringComponent],
   imports: [
     CommonModule,
-    HarvardArcAppsModule
+    HarvardArcAppsModule,
   ],
+  exports: [SleepMonitoringComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class InterventionModule { }
