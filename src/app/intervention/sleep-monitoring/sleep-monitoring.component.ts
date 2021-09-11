30e6637c88b85ba5bb2d99874d3efb07af073641
Mash <mashfiqui.r.s@gmail.com>
2021-09-11 12:33:55 -0700

    Saved sleep survey locally. This survey is used to generating sleep monitoring plot.
    Former-commit-id: 32eb2d772c01f5225d3d81264eb2037c13bcc6b5

--- src/app/intervention/sleep-monitoring/sleep-monitoring.component.ts
+++ src/app/intervention/sleep-monitoring/sleep-monitoring.component.ts
@@ -7,6 +7,8 @@ import { DatabaseService } from 'src/app/monitor/database.service';
 import * as Forge from 'node-forge';
 import { UserProfileService } from 'src/app/user/user-profile/user-profile.service';
 import { environment } from 'src/environments/environment';
+import * as moment from 'moment';
+import { EncrDecrService } from 'src/app/storage/encrdecrservice.service';
 
 @Component({
   selector: 'app-sleep-monitoring',
@@ -25,6 +27,7 @@ export class SleepMonitoringComponent implements OnInit {
   constructor(private menuCtrl:MenuController,
     private appUsageDb: DatabaseService,
     private userProfileService: UserProfileService,
+    private EncrDecr: EncrDecrService,
     private httpClient: HttpClient) { 
 
   }
@@ -65,8 +68,9 @@ export class SleepMonitoringComponent implements OnInit {
     //    https://medium.com/@DannyAziz97/rsa-encryption-with-js-python-7e031cbb66bb
     //    https://github.com/digitalbazaar/forge/blob/master/README.md#rsa
 
-    // {"user_id": this.userProfileService.username, "sleep_data": {"report_date": "20210901", "start": "1:30", "end": "08:00"}}
-    let requestDataJson = {"user_id": this.userProfileService.username}
+    //let requestDataJson = {"user_id": this.userProfileService.username, "sleep_data": {"report_date": "20210907", "start": "1:30", "end": "07:30"}}
+    var requestDataJson = {"user_id": this.userProfileService.username};
+    requestDataJson = this.addTodaysSleepDataIfAvailable(requestDataJson);
     let requestData2 = this.encryptWithPublicKey(JSON.stringify(requestDataJson));
     console.log("Encrypted: " + requestData2)
     console.log("Encrypted (encoded): " + encodeURIComponent(requestData2))
@@ -76,6 +80,34 @@ export class SleepMonitoringComponent implements OnInit {
 
     //let params2 = new HttpParams().set("requestData", encodeURIComponent('{"test":"holla"}'))
     return this.httpClient.get(imageUrl, { responseType: 'blob', 'headers': headers, 'params': params});
+  }
+
+  addTodaysSleepDataIfAvailable(requestDataJson) {
+    /*
+    If sleep data is available for today, then add is to the request
+    Otherwise, return the same object
+    */
+
+    if (window.localStorage['localSurvey'] != undefined){
+        let locallyStoredSurvey = JSON.parse(window.localStorage.getItem('localSurvey'));
+        if (locallyStoredSurvey.hasOwnProperty('sleep_survey')) {
+          if(locallyStoredSurvey['sleep_survey']["date"] == moment().format('YYYYMMDD')){
+            //decrypt the data and add to the requestDataJson
+            var decrypted = this.EncrDecr.decrypt(locallyStoredSurvey['sleep_survey']["encrypted"], environment.encyptString);
+            let decryptedSleepSurvey = JSON.parse(decrypted);
+            // console.log("decryptedSleepSurvey: " + decrypted);
+
+            //"sleep_data": {"report_date": "20210907", "start": "1:30", "end": "07:30"}
+            var todaysSleepData = {}
+            todaysSleepData["report_date"] = moment().format('YYYYMMDD');
+            todaysSleepData["start"] = decryptedSleepSurvey["Q2_modified"].split(" ")[0];
+            todaysSleepData["end"] = decryptedSleepSurvey["Q3_modified"].split(" ")[0];
+            requestDataJson["sleep_data"] = todaysSleepData;
+          } 
+        }
+    }
+    console.log("requestDataJson: " + JSON.stringify(requestDataJson));
+    return requestDataJson;
   }
 
   createImageFromBlob(image: Blob) {
