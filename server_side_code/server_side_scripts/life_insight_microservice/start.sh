ab9aafbda1a3345f7864962ef8f87091ff56ad6e
ms2749 <mashfiqui.rabbi@gmail.com>
2022-02-09 14:50:49 -0800

    Removed unsorted server side scripts

--- server_side_code/server_side_scripts/life_insight_microservice/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-app="life.insight.microservice"
-docker build -t ${app} .
-docker run -d -p 5100:5100 --name=${app}  -e AWS_ACCESS_KEY_ID=AKIA4HORFG5PTAOZGMWN -e AWS_SECRET_ACCESS_KEY=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 ${app}
