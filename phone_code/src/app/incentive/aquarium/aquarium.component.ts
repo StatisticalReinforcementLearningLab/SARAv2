90db1f819546e8cca59f8ea329881fc7d5fee99b
ms2749 <ms3749@cornell.edu>
2023-10-30 07:53:45 -0700

    Added NW logo. Changed name to adapts

--- phone_code/src/app/incentive/aquarium/aquarium.component.ts
+++ phone_code/src/app/incentive/aquarium/aquarium.component.ts
@@ -232,8 +232,8 @@ export class AquariumComponent implements OnInit {
         */
 
 
-        //this.title = "ADAPTS";
-        this.title = "SARA";
+        this.title = "ADAPTS";
+        //this.title = "SARA";
         console.log("aquarium.component.ts --- start");
         //this.menu.enable(true);
 
